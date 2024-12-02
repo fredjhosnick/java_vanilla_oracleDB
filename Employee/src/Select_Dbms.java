a74b772b22d329e3925d4e6775b270292cdbcad2
fredjhosnick <36901848+fredjhosnick@users.noreply.github.com>
2024-12-02 19:42:30 +0100

    Update Select_Dbms.java

--- Employee/src/Select_Dbms.java
+++ Employee/src/Select_Dbms.java
@@ -9,7 +9,7 @@ public class Select_Dbms {
 		try
 		{
 			Class.forName("oracle.jdbc.driver.OracleDriver");
-			Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","sys as sysdba","11Hola@migo11");
+			Connection conn = DriverManager.getConnection("jdbc:oracle:thin:@localhost:1521:orcl","sys as sysdba","passwordorcldb");
 			if(!conn.isClosed())
 			{
 				System.out.println("Successfully connected to Oracle server ...");
